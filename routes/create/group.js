35d765e5053a12b7831f7b8a259716e1e8c0ea31
Manishsingh1110 <dsmanishsingh@gmail.com>
2023-10-13 13:48:16 +0530

    Manihs

--- routes/create/group.js
+++ routes/create/group.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Group = require('../models/Group');
+const Group = require('../../models/group');
 
 // Create a route to create a new group
 router.post('/', async (req, res) => {
