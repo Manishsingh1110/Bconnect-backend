9b129cfa294878640272c38bba244a23c3e9aad6
Manishsingh1110 <dsmanishsingh@gmail.com>
2023-10-20 11:33:25 +0530

    user done

--- routes/create/user.js
+++ routes/create/user.js
@@ -2,53 +2,26 @@ const bcrypt = require('bcryptjs');
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
-const path = require('path');
-const fs = require('fs');
-const  User  = require('../../models/user');
+const User = require('../../models/user');
 const bodyParser = require('body-parser');
-router.use(bodyParser.json());
-router.use(bodyParser.urlencoded({ extended: false }));
-
-// const uploadDirectory = 'uploads/avatars';
-//
-// if (!fs.existsSync(uploadDirectory)) {
-//   fs.mkdirSync(uploadDirectory, { recursive: true });
-// }
-//
-// const storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     const username = req.body.username;
-//     const userUploadsDir = path.join(uploadDirectory, username);
-//
-//     if (!fs.existsSync(userUploadsDir)) {
-//       fs.mkdirSync(userUploadsDir, { recursive: true });
-//     }
-//
-//     cb(null, userUploadsDir);
-//   },
-//   filename: function (req, file, cb) {
-//     const extname = path.extname(file.originalname);
-//     cb(null, 'avatar_' + Date.now() + extname);
-//   },
-// });
-//
-// const upload = multer({ storage });
-//
-const firebaseApp = require('firebase/app')
+const firebaseApp = require('firebase/app');
 const firebaseStorage = require('firebase/storage');
-const config = require("../../config")
-//Initialize a firebase application
+const config = require('../../config');
+
+// Initialize a firebase application
 firebaseApp.initializeApp(config.firebaseConfig);
 
 // Initialize Cloud Storage and get a reference to the service
 const storage = firebaseStorage.getStorage();
 
-// Setting up multer as a middleware to grab photo uploads
 const upload = multer({ storage: multer.memoryStorage() });
+// Middleware for parsing JSON and URL-encoded data
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: false }));
 
 router.post('/', upload.single('avatar'), async (req, res) => {
   try {
-    console.log("Processing a request to Register");
+    console.log('Processing a request to Register');
     const {
       username,
       email,
@@ -60,56 +33,46 @@ router.post('/', upload.single('avatar'), async (req, res) => {
       lastname,
       description,
       profileType,
-      aboutus
+      aboutus,
     } = req.body;
-    console.log(req.body)
 
     if (!username || !email || !password || !companyname || !companyscale) {
-      if (req.file) {
-        const filePath = req.file.path;
-        fs.unlinkSync(filePath);
-      }
-      const userUploadsDir = path.join(uploadDirectory, username);
-      rimraf.sync(userUploadsDir);
-      return res.status(400).json({ msg: "Not all required fields have been entered" });
+      return res.status(400).json({ msg: 'Not all required fields have been entered' });
     }
 
     if (password.length < 6) {
-      if (req.file) {
-        const filePath = req.file.path;
-        fs.unlinkSync(filePath);
-      }
-      const userUploadsDir = path.join(uploadDirectory, username);
-      rimraf.sync(userUploadsDir);
-      return res.status(400).json({ msg: "Password is too short." });
+      return res.status(400).json({ msg: 'Password is too short.' });
     }
 
     const usernameTaken = await User.findOne({ username });
     if (usernameTaken) {
-      if (req.file) {
-        const filePath = req.file.path;
-        fs.unlinkSync(filePath);
-      }
-      const userUploadsDir = path.join(uploadDirectory, username);
-      rimraf.sync(userUploadsDir);
-      return res.status(409).json({ msg: "This username is already taken." });
+      return res.status(409).json({ msg: 'This username is already taken.' });
     }
 
     const emailTaken = await User.findOne({ email });
     if (emailTaken) {
-      if (req.file) {
-        const filePath = req.file.path;
-        fs.unlinkSync(filePath);
-      }
-      const userUploadsDir = path.join(uploadDirectory, username);
-      rimraf.sync(userUploadsDir);
-      return res.status(409).json({ msg: "This email is already taken." });
+      return res.status(409).json({ msg: 'This email is already taken.' });
     }
 
     const salt = await bcrypt.genSalt();
     const passwordHash = await bcrypt.hash(password, salt);
 
-    const avatar = req.file ? req.file.path : '';
+    let avatarURL = '';
+
+    const dateTime = giveCurrentDateTime();
+
+    const storageRef = firebaseStorage.ref(storage, `ProfilePic/${req.file.originalname + ' ' + dateTime}`);
+
+    // Create file metadata including the content type
+    const metadata = {
+      contentType: 'image/png',
+    };
+
+    // Upload the file to Firebase Storage
+    const snapshot = await firebaseStorage.uploadBytesResumable(storageRef, req.file.buffer, metadata);
+
+    // Grab the public URL
+    avatarURL = await firebaseStorage.getDownloadURL(snapshot.ref);
 
     const newCustomer = new User({
       username,
@@ -117,61 +80,28 @@ router.post('/', upload.single('avatar'), async (req, res) => {
       password: passwordHash,
       companyname,
       companyscale,
-      Products:JSON.parse(Products),
+      Products: JSON.parse(Products),
       firstname,
       lastname,
       description,
-      avatar,
+      avatar: avatarURL, // Set the avatar to the Firebase Storage URL
       profileType,
-      aboutus
+      aboutus,
     });
 
     const savedCustomer = await newCustomer.save();
     return res.status(201).json({ savedCustomer });
   } catch (error) {
-    console.error(error);
-    if (req.file) {
-      const filePath = req.file.path;
-      fs.unlinkSync(filePath);
-    }
-    const userUploadsDir = path.join(uploadDirectory, req.body.username);
-    rimraf.sync(userUploadsDir); // Delete the user's directory
-    return res.status(500).json({ msg: "Server error" });
+    return res.status(500).json({ msg: 'Server error' });
   }
 });
+
 const giveCurrentDateTime = () => {
-    const today = new Date();
-    const date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
-    const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-    const dateTime = date + ' ' + time;
-    return dateTime;
-}
-router.post("/pic", upload.single("profilepic"), async (req, res) => {
-    try {
-        const dateTime = giveCurrentDateTime();
-
-        const storageRef = firebaseStorage.ref(storage, `ProfilePic/${req.file.originalname + "       " + dateTime}`);
-
-        // Create file metadata including the content type
-        const metadata = {
-            contentType: 'image/png',
-        };
-
-        // Upload the file in the bucket storage
-        const snapshot = await firebaseStorage.uploadBytesResumable(storageRef, req.file.buffer, metadata);
-        //by using uploadBytesResumable we can control the progress of uploading like pause, resume, cancel
-
-        // Grab the public url
-        const downloadURL = await firebaseStorage.getDownloadURL(snapshot.ref);
-
-        // console.log('File successfully uploaded.');
-        return res.send({
-            message: 'file uploaded to firebase storage',
-            downloadURL: downloadURL
-        })
-    } catch (error) {
-        return res.status(400).send(error.message)
-    }
-});
+  const today = new Date();
+  const date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+  const time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
+  const dateTime = date + ' ' + time;
+  return dateTime;
+};
 
 module.exports = router;
