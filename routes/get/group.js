36aa618fc1c7a604c57585a87c25d9159fe1cc6f
Manishsingh1110 <dsmanishsingh@gmail.com>
2023-11-28 15:06:26 +0530

    fdfd

--- routes/get/group.js
+++ routes/get/group.js
@@ -5,17 +5,17 @@ const Group = require('../../models/group');
 // Create a route to get a random selection of up to 40 groups
 router.get('/', async (req, res) => {
   try {
-    // Fetch random groups and populate all fields for the referenced documents
+    // Fetch random groups without populating
     const randomGroups = await Group.aggregate([{ $sample: { size: 40 } }])
-    .exec();
+      .exec();
 
-  // Use populate for each reference field individually
-  await Group.populate(randomGroups, { path: 'creator' });
-  await Group.populate(randomGroups, { path: 'admins' });
-  await Group.populate(randomGroups, { path: 'members' });
-  await Group.populate(randomGroups, { path: 'posts' });
+    // Use populate for each reference field individually on the resulting array
+    const populatedGroups = await Group.populate(randomGroups, { path: 'creator' });
+    await Group.populate(populatedGroups, { path: 'admins' });
+    await Group.populate(populatedGroups, { path: 'members' });
+    await Group.populate(populatedGroups, { path: 'posts' });
 
-    res.status(200).json(randomGroups);
+    res.status(200).json(populatedGroups);
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Server error' });
