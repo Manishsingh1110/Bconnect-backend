258f9656eb1085dc36315c9e7cd03f176f2a3a91
Manishsingh1110 <dsmanishsingh@gmail.com>
2023-11-28 14:52:41 +0530

    getttt

--- routes/get/group.js
+++ routes/get/group.js
@@ -5,11 +5,19 @@ const Group = require('../../models/group');
 // Create a route to get a random selection of up to 40 groups
 router.get('/', async (req, res) => {
   try {
-    const randomGroups = await Group.find()
+    // Fetch random groups and populate all fields for the referenced documents
+    const randomGroups = await Group.aggregate([{ $sample: { size: 40 } }])
+      .populate('creator')
+      .populate('admins')
+      .populate('members')
+      .populate('posts')
+      .exec();
+
     res.status(200).json(randomGroups);
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Server error' });
   }
 });
+
 module.exports = router;
